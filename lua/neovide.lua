f397cf03b453851c34210a85d338599002245294
danferns <dannywritescode@gmail.com>
2024-01-22 15:43:41 +0530

    lower neovide font size

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "Fira Code:h12"
+  vim.o.guifont = "Fira Code:h11"
   vim.g.neovide_fullscreen = false
   vim.g.neovide_padding_top = 5
   vim.g.neovide_padding_bottom = 5
