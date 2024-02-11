86d2ffe37c02be600ab0584c60f1905127795945
danferns <dannywritescode@gmail.com>
2024-02-11 20:44:07 +0530

    configure svelte lsp and treesitter

--- init.lua
+++ init.lua
@@ -138,7 +138,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'svelte', 'vimdoc', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -284,6 +284,7 @@ local servers = {
   pyright = {},
   rust_analyzer = {},
   tsserver = {},
+  svelte = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
