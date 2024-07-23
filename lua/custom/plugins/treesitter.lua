2c504c76e17a76c80ddc24cbfe7a96d8675e269f
danferns <dannywritescode@gmail.com>
2024-07-23 16:44:41 +0530

    add nushell treesitter

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    { "nushell/tree-sitter-nu" },
   },
   build = ':TSUpdate',
 }
