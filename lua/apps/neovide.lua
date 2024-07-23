2392750c5759594d35875fe1726f552c98406319
danferns <dannywritescode@gmail.com>
2024-07-23 16:43:18 +0530

    config neovide

--- lua/apps/neovide.lua
+++ lua/apps/neovide.lua
@@ -1,9 +1,18 @@
 if vim.g.neovide then
-  -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "Fira Code:h11"
-  vim.g.neovide_fullscreen = false
-  vim.g.neovide_padding_top = 5
-  vim.g.neovide_padding_bottom = 5
-  vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.o.guifont = "Fira Code:h12"
+  -- Main Window Settings
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  -- Editor Settings
+  vim.g.neovide_scroll_animation_length = 0.1
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_floating_shadow = false
+  -- Cursor Settings
+  vim.g.neovide_cursor_animation_length = 0.15
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_trail_size = 0.1
+  vim.g.neovide_cursor_vfx_mode = "ripple"
+  vim.g.neovide_cursor_vfx_opacity = 75.0
+  vim.g.neovide_cursor_unfocused_outline_width = 0.04
 end
