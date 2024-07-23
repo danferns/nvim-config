25f200e17add38dd8d83ee3e93fd2dc63e9c68fd
danferns <dannywritescode@gmail.com>
2024-07-23 16:44:00 +0530

    rose pine lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'catppuccin',
+      theme = 'rose-pine',
       component_separators = '',
       section_separators = '',
     },
